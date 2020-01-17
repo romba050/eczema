efbdfb7b8566d1c8c21c99635a0278c37ccd35c2
bzyl <basilerommes@hotmail.com>
2020-01-17 13:16:30 +0100

    gitignore updated

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from opentargets import OpenTargetsClient
 import operator
+from tqdm import tqdm
+
 ot = OpenTargetsClient()
 
 # search for all targets associated with the given disease
 eczema_targets = ot.get_associations_for_disease('EFO_0000274') # atopic eczema
-print "Found " + str(len(eczema_targets)) + " possible associations"
+print("Found " + str(len(eczema_targets)) + " possible associations")
 
 target_by_id_dict = {}
 for target in eczema_targets:
         target_by_id_dict[target["target"]["id"]] = target
 
-# known AD targets provided by Leo Pharma
+# known atopic dermatitis (AD) targets provided by Leo Pharma
 known_targets = ["ENSG00000178394", "ENSG00000135914", "ENSG00000043591", "ENSG00000106546", "ENSG00000088832", \
                  "ENSG00000118432", "ENSG00000163823", "ENSG00000005844", "ENSG00000092009", "ENSG00000073756", \
                  "ENSG00000007908", "ENSG00000131981", "ENSG00000107485", "ENSG00000113580", "ENSG00000196639", \
@@ -31,26 +33,26 @@ known_targets = ["ENSG00000178394", "ENSG00000135914", "ENSG00000043591", "ENSG0
                  "ENSG00000125910", "ENSG00000198121", "ENSG00000064547", "ENSG00000171517", "ENSG00000171608"]
 
 # find related diseases
-diseaseDict = {}
-for target_id in known_targets:
-    print "Getting know diseases for target " + target_id
+diseaseDict = {}  # disease id - total association score
+for target_id in tqdm(known_targets):
+    print("Getting know diseases for target " + target_id)
     for acc_target in ot.get_associations_for_target(target_id):
         if acc_target["disease"]["id"] is not "other":
             if acc_target["disease"]["id"] not in diseaseDict:
                 diseaseDict[acc_target["disease"]["id"]] = 0
             diseaseDict[acc_target["disease"]["id"]] += acc_target["association_score"]["overall"]
-print "Found " + str(len(diseaseDict.keys())) + " related diseases"
+print("Found " + str(len(diseaseDict.keys())) + " related diseases")
 
 # sort diseases according to relevance
 disease_array = sorted(diseaseDict.items(), key=operator.itemgetter(1))
-disease_array.reverse()
+disease_array.reverse()  # sort in order of descending relevance
 
 # find potential targets
 second_target_dict = {}
 diseases_process = 0;
 for disease in disease_array:
     disease_id = disease[0]
-    print "Getting targets for disease " + disease_id + " (" + str(disease[1]) + ")"
+    print("Getting targets for disease " + disease_id + " (" + str(disease[1]) + ")")
     diseases_process = diseases_process + 1
     if diseases_process > 50:
         break
@@ -75,9 +77,9 @@ for target_tuple in potential_targets_filtered:
         tuples_printed = tuples_printed +1
         if tuples_printed > 500:
             break
-        print target_tuple[0] + "|" + str(target_tuple[1]) + "|" + \
+        print(target_tuple[0] + "|" + str(target_tuple[1]) + "|" + \
               (target_by_id_dict[target_tuple[0]]["target"]["gene_info"]["symbol"] + \
                 "|" + target_by_id_dict[target_tuple[0]]["target"]["gene_info"]["name"] + \
                 "|" + str(target_by_id_dict[target_tuple[0]]["association_score"]["overall"]) + "|" + \
                 str(target_by_id_dict[target_tuple[0]]["association_score"]["datatypes"]["literature"]) \
-               if target_tuple[0] in target_by_id_dict else "|")
+               if target_tuple[0] in target_by_id_dict else "|"))
